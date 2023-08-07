7f2203ff5d1bf3143eb880e645273a04fc0392f4
Ajay Sharma <ajaysh73@gmail.com>
2023-08-07 15:12:35 -0400

    First commit of the basic NextJs Blog application

---
+++ src/app/components/footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  console.log('hello');
+  return (
+    <div>
+      <div>©2023 SharmaIO. All rights reserved.</div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Footer;
