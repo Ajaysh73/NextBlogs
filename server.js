6381e9a0159f1bb7a5e832249bea5e267970f697
Ajay Sharma <ajaysh73@gmail.com>
2023-08-09 16:11:28 -0400

    another updated to server.js

--- server.js
+++ server.js
@@ -1,15 +1,36 @@
+// const { createServer } = require('http');
+// const next = require('next');
+
+// const port = process.env.PORT || 3000;
+// const dev = process.env.NODE_ENV !== 'production';
+// const app = next({ dev });
+// const handle = app.getRequestHandler();
+
+// app.prepare().then(() => {
+//   createServer((req, res) => {
+//     handle(req, res);
+//   }).listen(port, (err) => {
+//     if (err) throw err;
+//     console.log(`> Ready on http://localhost:${port}`);
+//   });
+// });
 const { createServer } = require('http');
+const { parse } = require('url');
 const next = require('next');
 
-const port = process.env.PORT || 3000;
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
+const port = process.env.PORT || 3000;
+
 app.prepare().then(() => {
-  createServer((req, res) => {
-    handle(req, res);
-  }).listen(port, (err) => {
+  const server = createServer((req, res) => {
+    const parsedUrl = parse(req.url, true);
+    handle(req, res, parsedUrl);
+  });
+
+  server.listen(port, (err) => {
     if (err) throw err;
     console.log(`> Ready on http://localhost:${port}`);
   });
