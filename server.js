395fc7bbd14967d4bbcc76d74c8e9b48f59b02a5
Ajay Sharma <ajaysh73@gmail.com>
2023-08-11 13:57:35 -0400

    updated server.js, package.json

--- server.js
+++ server.js
@@ -35,24 +35,56 @@
 //     console.log(`> Ready on http://localhost:${port}`);
 //   });
 // });
-const { createServer } = require('http');
-const { parse } = require('url');
+// const { createServer } = require('http');
+// const { parse } = require('url');
+// const next = require('next');
+
+// const dev = process.env.NODE_ENV !== 'production';
+// const app = next({ dev });
+// const handle = app.getRequestHandler();
+
+// const port = process.env.PORT || 3000;
+// console.log(`Port - ${port}`);
+// app.prepare().then(() => {
+//   const server = createServer((req, res) => {
+//     const parsedUrl = parse(req.url, true);
+//     handle(req, res, parsedUrl);
+//   });
+
+//   server.listen(port, (err) => {
+//     if (err) throw err;
+//     console.log(`> Ready on http://localhost:${port}`);
+//   });
+// });
+const express = require('express');
 const next = require('next');
 
 const dev = process.env.NODE_ENV !== 'production';
+const port = process.env.PORT || 3000;
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
-const port = process.env.PORT || 3000;
-console.log(`Port - ${port}`);
-app.prepare().then(() => {
-  const server = createServer((req, res) => {
-    const parsedUrl = parse(req.url, true);
-    handle(req, res, parsedUrl);
-  });
+app
+  .prepare()
+  .then(() => {
+    const server = express();
 
-  server.listen(port, (err) => {
-    if (err) throw err;
-    console.log(`> Ready on http://localhost:${port}`);
+    server.get('/p/:id', (req, res) => {
+      const actualPage = '/post';
+      const queryParams = { title: req.params.id };
+      app.render(req, res, actualPage, queryParams);
+    });
+
+    server.get('*', (req, res) => {
+      return handle(req, res);
+    });
+
+    server.listen(port, (err) => {
+      if (err) throw err;
+      console.log('> Ready on http://localhost:' + port);
+    });
+  })
+  .catch((ex) => {
+    console.error(ex.stack);
+    process.exit(1);
   });
-});
