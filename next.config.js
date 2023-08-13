242f380159733f8c246f3b14e8f026fa02559785
Ajay Sharma <ajaysh73@gmail.com>
2023-08-13 15:49:26 -0400

    Created Hero page

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ['images.pexels.com', 'example.com'],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
