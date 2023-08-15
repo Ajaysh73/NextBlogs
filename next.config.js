599397ffa48f2336db78df408b466fab96dfe8ff
Ajay Sharma <ajaysh73@gmail.com>
2023-08-15 09:04:17 -0400

    updated next.config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  images: {
-    domains: ['images.pexels.com', 'example.com'],
-  },
+  trailingSlash: true,
+  images: { unoptimized: true, domains: ['images.pexels.com', 'example.com'] },
 };
 module.exports = nextConfig;
