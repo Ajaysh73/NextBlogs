91f0dd386076caf9ec2284b4d5ce9646eee3ab48
Ajay Sharma <ajaysh73@gmail.com>
2023-08-15 14:14:51 -0400

    added added portfolio category page

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,8 +1,84 @@
 import React from 'react';
 import styles from './page.module.css';
+import Button from '@/components/Button/Button';
+import Image from 'next/image';
 
-const Category = () => {
-  return <div>Category</div>;
+const Category = ({ params }) => {
+  console.log(params);
+  return (
+    <div>
+      <h1 className={styles.catTitle}>{params.category}</h1>
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Creative Portfolio</h1>
+          <p className={styles.desc}>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur
+            vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate
+            ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet
+            fugiat tenetur?
+          </p>
+          <Button
+            text='See More'
+            url='#'
+          />
+        </div>
+        <div className={styles.imgContainer}>
+          <Image
+            className={styles.img}
+            fill={true}
+            src='https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg'
+            alt=''
+          />
+        </div>
+      </div>
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Minimal Single Product</h1>
+          <p className={styles.desc}>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur
+            vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate
+            ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet
+            fugiat tenetur
+          </p>
+          <Button
+            text='See More'
+            url='#'
+          />
+        </div>
+        <div className={styles.imgContainer}>
+          <Image
+            className={styles.img}
+            fill={true}
+            src='https://images.pexels.com/photos/2103127/pexels-photo-2103127.jpeg'
+            alt=''
+          />
+        </div>
+      </div>
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Strong Together Charity</h1>
+          <p className={styles.desc}>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur
+            vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate
+            ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet
+            fugiat tenetur
+          </p>
+          <Button
+            text='See More'
+            url='#'
+          />
+        </div>
+        <div className={styles.imgContainer}>
+          <Image
+            className={styles.img}
+            fill={true}
+            src='https://images.pexels.com/photos/2916450/pexels-photo-2916450.jpeg'
+            alt=''
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Category;
