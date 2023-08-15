1a861ba35b82e88fdca13f990acf765c1c0def1b
Ajay Sharma <ajaysh73@gmail.com>
2023-08-15 13:26:13 -0400

    Added portfolio page

--- src/app/components/footer/Footer.jsx
+++ src/app/components/footer/Footer.jsx
@@ -7,20 +7,14 @@ const Footer = () => {
     <div className={styles.container}>
       <div>©2023 SharmaIO. All rights reserved.</div>
       <div className={styles.social}>
-        {/* <Image
-          className={styles.icon}
-          width={15}
-          height={15}
-          src='/1.png'
-          alt='ajayIO'
-        /> */}
-        <img
+        <Image
           className={styles.icon}
           width={15}
           height={15}
           src='/1.png'
           alt='ajayIO'
         />
+
         <Image
           className={styles.icon}
           width={15}
