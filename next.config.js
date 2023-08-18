6c1318214e8b7e5b89d410a8930941ef30896ff9
Ajay Sharma <ajaysh73@gmail.com>
2023-08-18 16:08:08 -0400

    modified next.config.js for azure static webapp dep

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
-  trailingSlash: true,
   images: { unoptimized: true, domains: ['images.pexels.com', 'example.com'] },
 };
 module.exports = nextConfig;
