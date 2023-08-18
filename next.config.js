3b5e5607b3c28cb2ac844b67243edd241bca7e98
Ajay Sharma <ajaysh73@gmail.com>
2023-08-18 16:32:49 -0400

    standalone added

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   images: { unoptimized: true, domains: ['images.pexels.com', 'example.com'] },
 };
 module.exports = nextConfig;
