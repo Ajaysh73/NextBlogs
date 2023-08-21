31bda62f35483d050e9ef900ae4eda9d63a4c67f
Ajay Sharma <ajaysh73@gmail.com>
2023-08-21 16:37:37 -0400

    new changes

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -4,9 +4,12 @@ import Image from 'next/image';
 import { notFound } from 'next/navigation';
 
 async function getData(id) {
-  const res = await fetch(`http://localhost:3000/api/posts/${id}`, {
-    cache: 'no-store',
-  });
+  const res = await fetch(
+    `https://next-blogs-roan.vercel.app/api/posts/${id}`,
+    {
+      cache: 'no-store',
+    }
+  );
 
   if (!res.ok) {
     return notFound();
--- src/app/utils/db.js
+++ src/app/utils/db.js
@@ -2,7 +2,6 @@ import mongoose from 'mongoose';
 
 const connect = async () => {
   try {
-    console.log(process.env.MONGO);
     mongoose.set('strictQuery', false);
     await mongoose.connect(process.env.MONGO);
   } catch (error) {
