d1d748109ccc725142487a5edea965ad5dea9220
Ajay Sharma <ajaysh73@gmail.com>
2023-08-22 07:35:17 -0400

    updated BASE_URL

--- src/app/utils/apiUtils.js
+++ src/app/utils/apiUtils.js
@@ -2,6 +2,6 @@ export function getApiUrl(path) {
   if (process.env.NODE_ENV === 'development') {
     return `http://localhost:3000${path}`;
   } else {
-    return `${process.env.NEXT_PUBLIC_VERCEL_URL}${path}`;
+    return `${process.env.NEXT_PUBLIC_API_BASE_URL}${path}`;
   }
 }
